c8e32b2dcb638fbbdbcacbe31fcd4d0491c2baf4
nurf21 <nurfauzangiri@gmail.com>
2025-06-25 14:41:11 +0700

    Add wait function to demonstrate async behavior and handle promises

--- async-await/async-await.js
+++ async-await/async-await.js
@@ -16,3 +16,23 @@ async function loadJson(url) {
     console.log(err);
   }
 })();
+
+// Call async from non-async
+// ==================================================
+
+async function wait() {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  return 10;
+}
+
+function f() {
+  wait()
+    .then(result => {
+      // result === 10
+      console.log("Got:", result)
+    })
+    .catch(err => {
+      console.error("oops", err)
+    })
+}
